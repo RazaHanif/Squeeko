3b632aa5c6f3d37c1c8eec87789ecec9402e3b5f
Raza Hanif <raza_hanif@ymail.com>
2025-06-29 13:12:53 -0400

    Building the base @ 1:12:53 PM

--- back/controllers/authController.js
+++ back/controllers/authController.js
@@ -2,7 +2,37 @@
 
 // Create a Center -- takes in form from frontend
 export const registerCenter = async (req, res, next) => {
-    // TODO: Validate req.body (name, address, phone, email, supervisorName, regFee, latePaymentFee)
+    // TODO: Validate req.body (name, address, phone, email, supervisorName, regFee, latePickupFee)
+    
+    // Do some server side validation for info
+    const data = req.body
+
+    const name = data.name
+    // Check if name already exists in db, name must be unique
+
+    const address = data.address
+    // Check if address is real? 
+    // How to do that??
+
+    const phone = data.phone
+    // regex check for valid number
+
+    const email = data.email
+    // regex check for valid email
+
+    const supervisor = data.supervisor
+    // Check if name exists in users and has supervisor role
+
+    const registrationFee = data.registrationFee
+    const lateFee = data.lateFee
+    // Just check if the fees are valid prices
+
+    const fees = data.fees
+    // idk about this check, cuz like every center does it different
+    // maybe just check it exists as a key:value -- package1:$400
+
+
+
 }
 
 export const registerUser = async (req, res, next) => {
