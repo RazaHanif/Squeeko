59ef51daa01babb75044b26899d5b8d666344f0c
Raza Hanif <raza_hanif@ymail.com>
2025-06-29 12:19:25 -0400

    Building the base @ 12:19:25 PM

---
+++ back/app.js
@@ -0,0 +1,33 @@
+import express from 'express'
+import cors from 'cors'
+import helmet from 'helmet'
+import morgan from 'morgan'
+import config from './config/index'
+import authRoutes from './routes/authRoutes'
+
+const app = express()
+
+// Middleware
+
+app.use(express.json())
+app.use(cors())
+app.use(helemt())
+app.use(morgan('dev'))
+
+// Routes 
+app.get('/', (req, res) => {
+    res.status(200).send('Squeeko Status: OK')
+})
+
+app.use('/auth', authRoutes)
+
+app.use((err, req, res, next) => {
+    console.error(err)
+    res.status(500).send('oopsypoopsy')
+})
+
+const PORT = config.PORT
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+})
--- back/config/index.js
+++ back/config/index.js
@@ -1,15 +1,20 @@
 import dotenv from 'dotenv'
+import path from 'path'
+import { fileURLToPath } from 'url'
 
-dotenv.config()
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+dotenv.config({ path: path.resolve(__dirname, '../.env') })
 
 const config = {
-    port: parseInt(process.env.PORT) || 3000,
-    nodeEnv: process.env.NODE_ENV || 'development',
-    databaseUrl: process.env.DATABASE_URL,
-    redisUrl: process.env.REDIS_URL,
-    jwtSecret: process.env.JWT_SECRET,
+    PORT: parseInt(process.env.PORT || '3000', 10),
+    DATABASE_URL: process.env.DATABASE_URL,
+    JWT_SECRET: process.env.JWT_SECRET,
+    NODE_ENV: process.env.NODE_ENV || 'development',
 
-    // Cloud Storage
+    // Cloud Storage -- idk if i need this yet
+    // Will need to rename
     cloudStorage: {
         bucketName: process.env.CLOUD_STORAGE_BUCKET_NAME,
         region: process.env.CLOUD_STORAGE_REGION,
@@ -20,8 +25,13 @@ const config = {
 
 }
 
-if (!config.databaseUrl) {
+if (!config.DATABASE_URL) {
     console.error('FATAL ERROR: DATABASE_URL is not defined!')
+    process.exit(1)
+}
+
+if (!config.JWT_SECRET) {
+    console.error('FATAL ERROR: JWT_SECRET is not defined!')
     process.exit(1)
 }
 
