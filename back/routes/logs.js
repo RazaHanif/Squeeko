89cd028c44a44c3a000b80607a6382149e0732b5
Raza Hanif <raza_hanif@ymail.com>
2025-07-04 17:56:59 -0400

    Building the base @ 5:56:59 PM

--- back/routes/logs.js
+++ back/routes/logs.js
@@ -1,7 +1,11 @@
 import { Router } from 'express';
+import { updateDailyLog, viewDailyLog, viewDailyLogHistory } from '../controllers/logs';
 
 const router = Router();
 
-router.get('/documents/:childId', getAllFormsForChild)
+router.post('/logs', updateDailyLog)
+router.get('/logs', viewDailyLog)
+router.get('/logs/history', viewDailyLogHistory)
+
 
 export default router;
