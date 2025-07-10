3a021c4c1d6e6da5053bf4654a3944e8632dd0a6
Raza Hanif <raza_hanif@ymail.com>
2025-07-10 14:21:24 -0400

    Building the base @ 2:21:24 PM

--- back/controllers/staff.js
+++ back/controllers/staff.js
@@ -1,4 +1,5 @@
 import prisma from '../db/prisma'
+import { staffSafeSelect } from '../utils/prismaSelects'
 
 import config from '../config/index'
 
@@ -6,6 +7,30 @@ import config from '../config/index'
 // Get all staff for a given center
 export const getAllStaff = async (req, res, next) => {
     try {
+        const centerId = req.body.centerId
+
+        if (!centerId) {
+            return res.status(400).json({
+                error: 'Invalid Center ID'
+            })
+        }
+
+        const center = prisma.staff.findMany({
+            where: {
+                centerId: centerId
+            },
+            select: staffSafeSelect
+        })
+
+        if (!center) {
+            return res.status(404).json({
+                error: 'No Center found'
+            })
+        }
+
+        return res.status(200).json({
+            staff: center.staff
+        })
     
     } catch (err) {
         console.log(err)
---
+++ back/utils/prismaSelects.js
@@ -0,0 +1,12 @@
+export const staffSafeSelect = {
+  id: true,
+  firstName: true,
+  lastName: true,
+  email: true,
+  cprRenewalDate: true,
+  eceLicenseExpiryDate: true,
+  tbTestDate: true,
+  vaccinesList: true,
+  policeRecordCheckDate: true,
+  offenseDeclarationDate: true
+};
