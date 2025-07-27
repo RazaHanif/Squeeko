400ee45390fa1d188a1156027b492c8f68de71dd
Raza Hanif <raza_hanif@ymail.com>
2025-07-27 11:34:59 -0400

    Starting betterAuth @ 11:34:59 AM

--- back/auth.ts
+++ back/auth.ts
@@ -5,5 +5,8 @@ import { PrismaClient } from "@prisma/client"
 
 const prisma = new PrismaClient()
 export const auth = betterAuth({
-    database: prismaAdapter(prisma, { provider: 'postgresql' })
+    database: prismaAdapter(prisma, { provider: 'postgresql' }),
+    emailAndPassword: {
+        enabled: true,
+    }
 })
