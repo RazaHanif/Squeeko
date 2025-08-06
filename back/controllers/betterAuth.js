34d2bb864f59148a41e29702e3106beb69851b5a
Raza Hanif <raza_hanif@ymail.com>
2025-08-06 13:25:30 -0400

    working on betterAuth integration @ 1:25:30 PM

--- back/controllers/betterAuth.js
+++ back/controllers/betterAuth.js
@@ -9,7 +9,7 @@ import config from '../config/index'
 
 export const signUp = async (req, res, next) => {
     try {
-        const { email, password, first_name, middle_name, last_name, center_id, role, address, phone_number } = req.body
+        const { email, password, first_name, middle_name, last_name, center_id, role, address, phone_number, alt_phone_number } = req.body
 
         // Validate Email
         const checkEmail = validateEmail(email)
@@ -17,6 +17,16 @@ export const signUp = async (req, res, next) => {
         if(!checkEmail) { 
             return res.status(400).json({
                 error: 'Invalid Email'
+            })
+        }
+
+        // Validate Phone Number
+        if (
+            !validatePhone(phone_number) ||
+            (alt_phone_number && !validatePhone(alt_phone_number))
+        ) {
+            return res.status(400).json({
+                error: 'Invalid Phone Number'
             })
         }
 
@@ -31,22 +41,29 @@ export const signUp = async (req, res, next) => {
             })
         }
 
+        // Create user with BetterAuth
         const { user } = await auth.api.signUp.email({
             body: {
                 email,
                 password,
                 first_name,
-                middle_name,
+                middle_name: middle_name || null,
                 last_name,
                 role
             }
         })
 
+        // Create Parent or Staff model
         if (role === 'PARENT') {
             await prisma.parent.create({
                 data: {
                     user_id: user.id,
-                    center_id: centerId
+                    center_id,
+                    address,
+                    phone_number,
+                    alt_phone_number: alt_phone_number || null,
+                    employer: employer || null,
+                    
                 }
             })
         }
