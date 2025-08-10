2b381f0864c4e15f3b0bff615f3ac170398c01cf
Raza Hanif <raza_hanif@ymail.com>
2025-08-10 13:39:55 -0400

    Working on messages webhook socket @ 1:39:55 PM

--- back/socket/message.socket.js
+++ back/socket/message.socket.js
@@ -16,6 +16,8 @@ model Message {
 export default messageSocketHandler = (io) => {
     io.on('connection', (socket) => {
         console.log(`User connected: ${socket.id}`)
+
+        // 
     })
 
     socket.on('send_message', async (data) => {
