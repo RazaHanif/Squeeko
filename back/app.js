08e754af9d7660775c0734ea7af869e8dcdfec2d
Raza Hanif <raza_hanif@ymail.com>
2025-08-10 14:05:50 -0400

    Working on messages webhook socket @ 2:05:50 PM

--- back/app.js
+++ back/app.js
@@ -8,7 +8,6 @@ import helmet from 'helmet'
 import morgan from 'morgan'
 import config from './config/index'
 import authRoutes from './routes/auth'
-import cookieParser from 'cookie-parser'
 import { messageSocketHandler } from './socket/message.socket'
 
 const app = express()
@@ -22,7 +21,6 @@ app.use(express.json())
 app.use(cors())
 app.use(helmet())
 app.use(morgan('dev'))
-app.use(cookieParser())
 
 // Routes 
 app.get('/', (req, res) => {
@@ -49,6 +47,6 @@ messageSocketHandler(io)
 
 const PORT = config.PORT
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
 })
