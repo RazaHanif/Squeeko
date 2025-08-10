63d1d66810df320506568b454a2b0afb75c23781
Raza Hanif <raza_hanif@ymail.com>
2025-08-10 13:34:37 -0400

    Working on messages webhook socket @ 1:34:36 PM

--- back/socket/message.socket.js
+++ back/socket/message.socket.js
@@ -1,11 +1,41 @@
 import prisma from '../db/prisma'
 
+/* 
+model Message {
+  id          String   @id @default(cuid())
+  sender      User     @relation("SentMessages", fields: [sender_id], references: [id])
+  sender_id   String
+  receiver    User     @relation("ReceivedMessages", fields: [receiver_id], references: [id])
+  receiver_id String
+  content     String?
+  image_url   String?
+  created_at  DateTime @default(now())
+}
+*/
+
 export default messageSocketHandler = (io) => {
     io.on('connection', (socket) => {
         console.log(`User connected: ${socket.id}`)
     })
 
     socket.on('send_message', async (data) => {
+        try {
+            const { sender_id, receiver_id, content, image_url } = data
+
+            const message = await prisma.message.create({
+                data: {
+                    sender_id,
+                    receiver_id,
+                    content: content || null,
+                    image_url: image_url || null
+                }
+            })
+        }
+
         
+
+        catch (err) {
+            console.log(err)
+        }
     })
 }
